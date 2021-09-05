0e67a51527fa1bf283e7c59c57ea3e29a99d8fd6
Hramov <trykhramov@gmail.com>
2021-09-05 19:35:46 +0700

    Fixed docker scratch problem

--- server/build.sh
+++ server/build.sh
@@ -1,2 +1,2 @@
+CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o ecograd_server .
 docker build -t ecograd_server .
-docker run --rm -it --name ecograd_server -p 5000:5000 ecograd_server
