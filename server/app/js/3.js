0fae65d3e29c4e880be3d4ad44834fd262cb83e7
hramov <trykhramov@gmail.com>
2022-04-24 12:05:14 +0700

    Fixin

---
+++ server/app/js/3.js
@@ -0,0 +1,63 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
+
+/***/ "./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/ts-loader/index.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/views/Expertise.vue?vue&type=script&lang=ts":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/cache-loader/dist/cjs.js??ref--14-0!./node_modules/babel-loader/lib!./node_modules/ts-loader??ref--14-2!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1!./src/views/Expertise.vue?vue&type=script&lang=ts ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ \"./node_modules/vue/dist/vue.runtime.esm-bundler.js\");\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"defineComponent\"])({}));\n\n//# sourceURL=webpack:///./src/views/Expertise.vue?./node_modules/cache-loader/dist/cjs.js??ref--14-0!./node_modules/babel-loader/lib!./node_modules/ts-loader??ref--14-2!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1");
+
+/***/ }),
+
+/***/ "./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader-v16/dist/templateLoader.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/views/Expertise.vue?vue&type=template&id=1b4a58c7":
+/*!**************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/babel-loader/lib!./node_modules/vue-loader-v16/dist/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1!./src/views/Expertise.vue?vue&type=template&id=1b4a58c7 ***!
+  \**************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: render */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ \"./node_modules/vue/dist/vue.runtime.esm-bundler.js\");\n\n\nvar _hoisted_1 = /*#__PURE__*/Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"createStaticVNode\"])(\"<section class=\\\"expertise\\\"><div><div class=\\\"text-center\\\" style=\\\"margin:20px 0;\\\"><h2>Нормативная база</h2></div><div class=\\\"row\\\"><div class=\\\"col-lg-8\\\"><ul class=\\\"list-group\\\"><a class=\\\"list-group-item\\\" href=\\\"#\\\"> О ВНЕСЕНИИ ИЗМЕНЕНИЙ В ГРАДОСТРОИТЕЛЬНЫЙ КОДЕКС РОССИЙСКОЙ ФЕДЕРАЦИИ И ОТДЕЛЬНЫЕ ЗАКОНОДАТЕЛЬНЫЕ АКТЫ РОССИЙСКОЙ ФЕДЕРАЦИИ от 28 ноября 2011 года (337-ФЗ) </a><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Постановление правительства №145 от 5 марта 2007 года </a><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Постановление правительства №87 от 16 февраля 2008 года </a><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Форма заключения на проектную документацию </a></ul></div><div class=\\\"col-lg-4\\\" style=\\\"padding:10px;\\\"><img style=\\\"width:100%;\\\" src=\\\"assets/img/zakon.jpg\\\" alt=\\\"Zakon\\\" data-aos=\\\"zoom-out\\\" data-aos-delay=\\\"100\\\"></div></div></div><div><div class=\\\"text-center\\\" style=\\\"margin:20px 0;\\\"><h2>Документы для проведения экспертизы</h2></div><div class=\\\"row\\\"><div class=\\\"col-lg-8\\\"><ul class=\\\"list-group\\\"><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Форма письма обращения для проведения негосударственной экспертизы проектной документации </a><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Приложение/опись к письму обращения для проведения негосударственной экспертизы </a><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Заявка на проведение негосударственной экспертизы проектной документации </a><a class=\\\"list-group-item\\\" href=\\\"#\\\">Анкета заявителя</a><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Акт приема передачи документов на экспертизу линейного объекта </a><a class=\\\"list-group-item\\\" href=\\\"#\\\"> Акт приема передачи документов на экспертизу объекта капитального строительства </a><a class=\\\"list-group-item\\\" href=\\\"#\\\">Доверенность</a><a class=\\\"list-group-item\\\" href=\\\"#\\\">Сведения об объекте</a></ul></div><div class=\\\"col-lg-4\\\" style=\\\"padding:10px;\\\"><img style=\\\"width:100%;\\\" src=\\\"assets/img/docs.jpg\\\" alt=\\\"Zakon\\\" data-aos=\\\"zoom-out\\\" data-aos-delay=\\\"100\\\"></div></div></div><br></section>\", 1);\n\nfunction render(_ctx, _cache, $props, $setup, $data, $options) {\n  return Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"openBlock\"])(), Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"createBlock\"])(\"div\", null, [_hoisted_1]);\n}\n\n//# sourceURL=webpack:///./src/views/Expertise.vue?./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/babel-loader/lib!./node_modules/vue-loader-v16/dist/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1");
+
+/***/ }),
+
+/***/ "./src/views/Expertise.vue":
+/*!*********************************!*\
+  !*** ./src/views/Expertise.vue ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Expertise_vue_vue_type_template_id_1b4a58c7__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Expertise.vue?vue&type=template&id=1b4a58c7 */ \"./src/views/Expertise.vue?vue&type=template&id=1b4a58c7\");\n/* harmony import */ var _Expertise_vue_vue_type_script_lang_ts__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Expertise.vue?vue&type=script&lang=ts */ \"./src/views/Expertise.vue?vue&type=script&lang=ts\");\n/* empty/unused harmony star reexport */\n\n\n_Expertise_vue_vue_type_script_lang_ts__WEBPACK_IMPORTED_MODULE_1__[\"default\"].render = _Expertise_vue_vue_type_template_id_1b4a58c7__WEBPACK_IMPORTED_MODULE_0__[\"render\"]\n/* hot reload */\nif (false) {}\n\n_Expertise_vue_vue_type_script_lang_ts__WEBPACK_IMPORTED_MODULE_1__[\"default\"].__file = \"src/views/Expertise.vue\"\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (_Expertise_vue_vue_type_script_lang_ts__WEBPACK_IMPORTED_MODULE_1__[\"default\"]);\n\n//# sourceURL=webpack:///./src/views/Expertise.vue?");
+
+/***/ }),
+
+/***/ "./src/views/Expertise.vue?vue&type=script&lang=ts":
+/*!*********************************************************!*\
+  !*** ./src/views/Expertise.vue?vue&type=script&lang=ts ***!
+  \*********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_cache_loader_dist_cjs_js_ref_14_0_node_modules_babel_loader_lib_index_js_node_modules_ts_loader_index_js_ref_14_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_Expertise_vue_vue_type_script_lang_ts__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/cache-loader/dist/cjs.js??ref--14-0!../../node_modules/babel-loader/lib!../../node_modules/ts-loader??ref--14-2!../../node_modules/cache-loader/dist/cjs.js??ref--0-0!../../node_modules/vue-loader-v16/dist??ref--0-1!./Expertise.vue?vue&type=script&lang=ts */ \"./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/ts-loader/index.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/views/Expertise.vue?vue&type=script&lang=ts\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return _node_modules_cache_loader_dist_cjs_js_ref_14_0_node_modules_babel_loader_lib_index_js_node_modules_ts_loader_index_js_ref_14_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_Expertise_vue_vue_type_script_lang_ts__WEBPACK_IMPORTED_MODULE_0__[\"default\"]; });\n\n/* empty/unused harmony star reexport */ \n\n//# sourceURL=webpack:///./src/views/Expertise.vue?");
+
+/***/ }),
+
+/***/ "./src/views/Expertise.vue?vue&type=template&id=1b4a58c7":
+/*!***************************************************************!*\
+  !*** ./src/views/Expertise.vue?vue&type=template&id=1b4a58c7 ***!
+  \***************************************************************/
+/*! exports provided: render */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_cache_loader_dist_cjs_js_ref_12_0_node_modules_babel_loader_lib_index_js_node_modules_vue_loader_v16_dist_templateLoader_js_ref_6_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_Expertise_vue_vue_type_template_id_1b4a58c7__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/cache-loader/dist/cjs.js??ref--12-0!../../node_modules/babel-loader/lib!../../node_modules/vue-loader-v16/dist/templateLoader.js??ref--6!../../node_modules/cache-loader/dist/cjs.js??ref--0-0!../../node_modules/vue-loader-v16/dist??ref--0-1!./Expertise.vue?vue&type=template&id=1b4a58c7 */ \"./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader-v16/dist/templateLoader.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/views/Expertise.vue?vue&type=template&id=1b4a58c7\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return _node_modules_cache_loader_dist_cjs_js_ref_12_0_node_modules_babel_loader_lib_index_js_node_modules_vue_loader_v16_dist_templateLoader_js_ref_6_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_Expertise_vue_vue_type_template_id_1b4a58c7__WEBPACK_IMPORTED_MODULE_0__[\"render\"]; });\n\n\n\n//# sourceURL=webpack:///./src/views/Expertise.vue?");
+
+/***/ })
+
+}]);
