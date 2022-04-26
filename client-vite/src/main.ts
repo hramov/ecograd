42101f7ab3e2b246a45ca18f2ecb4d81e215489c
hramov <trykhramov@gmail.com>
2022-04-26 20:48:25 +0700

    Added nest version of the server

--- client-vite/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
-import router from './router';
-import App from './App.vue';
-
-createApp(App).use(createPinia()).use(router).mount('#app');
