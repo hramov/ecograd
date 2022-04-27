de9795e8c8a0cd1d17aebca1ceec3776c062cca7
hramov <trykhramov@gmail.com>
2022-04-27 13:40:29 +0700

    Fixin

--- nest-server/dist/app.module.js
+++ nest-server/dist/app.module.js
@@ -55,7 +55,7 @@ AppModule = __decorate([
             typeorm_1.TypeOrmModule.forRoot({
                 type: 'postgres',
                 host: process.env.DB_HOST,
-                port: parseInt(process.env.DB_HOST),
+                port: parseInt(process.env.DB_PORT),
                 username: process.env.DB_USER,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -37,7 +37,7 @@ import { join } from 'path';
 		TypeOrmModule.forRoot({
 			type: 'postgres',
 			host: process.env.DB_HOST,
-			port: parseInt(process.env.DB_HOST),
+			port: parseInt(process.env.DB_PORT),
 			username: process.env.DB_USER,
 			password: process.env.DB_PASSWORD,
 			database: process.env.DB_NAME,
